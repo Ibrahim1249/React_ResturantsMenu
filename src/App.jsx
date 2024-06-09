600e3b5cea7f418e08626f1a0eceaedd223ef31d
Ibrahim1249 <mominibrahim98.im85@gmail.com>
2024-06-09 11:55:10 +0530

    started working resturant menu assignment work on navbar and footer component

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+
+
+import React from 'react'
+import Header from "./Header"
+import Footer from "./Footer"
+import "./App.css"
+
+function App() {
+  return (
+    <>
+     <Header />
+     <Footer/>
+    </>
+  )
+}
+
+export default App
